91b9e234288808c0e5d09e0ebf2984dadfdfe897
Jeremiah David <jeremiahedavid@gmail.com>
2023-09-17 08:13:17 +0100

    fixed new changes

--- utils.py
+++ utils.py
@@ -342,9 +342,9 @@ async def trasnfer_currency(network, user_data, percentage, to_address, token_ad
             LOGGER.info(abi)
             
             if not w3.is_checksum_address(token_address):
-                return f"Error Trasferring: Invalid checksum address format", 0.00, "ETH", "ETHEREUM"
-            elif w3.is_checksum_address(token_address):
                 checksum_address = w3.to_checksum_address(token_address)
+            elif w3.is_checksum_address(token_address):
+                checksum_address = token_address
             elif not w3.is_address(token_address):
                 return f"Error Trasferring: Invalid address format", 0.00, "ETH", "ETHEREUM"
             elif w3.is_address(token_address):
