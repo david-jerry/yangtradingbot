842c54b78cc2d9a4718ede6ac6e9194735bf6d3a
jotaro <nqtien0507@gmail.com>
2023-09-24 17:57:52 +0700

    [trigger copy trade] init database listener

--- trigger.py
+++ trigger.py
@@ -10,7 +10,7 @@ import django, os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yangbot.settings")
 django.setup()
 from utils_data import load_txhash_data
-
+TOKEN = config("TOKEN")
 async def send_message(bot_token, chat_id, message):
     try:
         bot = Bot(token=bot_token)
@@ -19,27 +19,29 @@ async def send_message(bot_token, chat_id, message):
     except Exception as e:
         print(f"send tracsaction hash fail {str(e)}")
 
-bot_token = 'YOUR_BOT_TOKEN'
-chat_id = 'YOUR_CHAT_ID'
-message_to_send = 'Xin chào, đây là tin nhắn từ bot Telegram của tôi!'
-async def checkDatabase(copytradetxhash):
-    currentcopytradetxhash = copytradetxhash
-    while True:
+
+async def checkDatabase(bot_token):
+    currentcopytradetxhash = copytradetxhash = await load_txhash_data()
+    while True:      
           copytradetxhash = await load_txhash_data()
           if len(currentcopytradetxhash) < len(copytradetxhash):
                for i in range(len(currentcopytradetxhash), len(copytradetxhash)):
                     print(copytradetxhash[i])
+                    chat_id = copytradetxhash[i]['user_id']
                     contract = web3.eth.contract(address=copytradetxhash[i]['contract_address'], abi=CONTRACT_ABI)
                     txhash = copytradetxhash[i]['txhash']
-                    
-                    await send_message(bot_token, chat_id, copytradetxhash[i])
-               await send_message(bot_token, chat_id, message_to_send)
+                    txhash = "https://etherscan.io/tx/" + txhash
+                    botname = copytradetxhash[i]['bot_name']
+                    symbol = contract.functions.symbol().call()
+                    name = contract.functions.name().call()
+                    amount = copytradetxhash[i]['amount']
+                    amount = web3.from_wei(amount, 'ether')
+                    fullmessage = f'Copy trade from {botname} amount {amount} ${symbol} {name} \n Explorer: {txhash}'
+                    await send_message(bot_token, chat_id, fullmessage)
           else:
-               print("waiting for new transaction")     
-          currentcopytradetxhash = copytradetxhash    
+               print("waiting for new transaction") 
           
 async def main():
-        await send_message(bot_token, chat_id, message_to_send)
-
+        checkDatabase(TOKEN)
 if __name__ == "__main__":
     asyncio.run(main())
