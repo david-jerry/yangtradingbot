380b09bf35f9335b74bc443bdf31015581f9fcec
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2023-09-30 13:57:18 +0700

    [Trade] Test price

---
+++ testprice.py
@@ -0,0 +1,89 @@
+import json
+from web3 import Web3
+import asyncio
+from decimal import Decimal
+from decouple import config
+from tasks import getprice
+import time 
+
+infura_url = config("PUBLICRPC")
+web3 = Web3(Web3.HTTPProvider(infura_url))
+import requests
+
+address = "0x8BF2405f5848db6dD2B8041456f73550c8d78E78"
+lastestBlock = web3.eth.get_block("latest")["number"]
+startBlock = 9737693
+import django, os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yangbot.settings")
+django.setup()
+ETHERAPI = config("ETHERAPI")
+ETHERSCAN_ENDPOINT = config("ETHERSCAN_ENDPOINT")
+UNISWAP_ROUTER =config("UNISWAP_ROUTER")
+UNIVERSAL_ROUTER =config("UNIVERSAL_ROUTER")
+UNISWAP_ABI = config("UNISWAP_ABI")
+DEXSCREENER_ENDPOINT = config("DEXSCREENER_ENDPOINT")
+WETH = config("WETH").lower()
+UNISWAP_FACTORY_ABI = config("UNISWAP_FACTORY_ABI")
+UNISWAP_FACTORY= config("UNISWAP_FACTORY").lower()
+from utils_data import load_token_addresses_all_from_trade_address
+from uniswap_universal_router_decoder import RouterCodec
+contract_abi = config("CONTRACT_ABI")
+
+async def log_loop(event_filter, poll_interval):
+    uniswap_factory_address = web3.to_checksum_address(UNISWAP_FACTORY)
+    uniswap_factory = web3.eth.contract(address=uniswap_factory_address, abi=UNISWAP_FACTORY_ABI)
+    weth = web3.to_checksum_address(WETH)
+    startblock = latest_block = web3.eth.get_block("latest")["number"]
+    while True:
+        try:
+
+            token_address = web3.to_checksum_address("0x00") 
+            #This line will give bug "An unexpected error occurred: Unknown format '0x00', attempted to normalize to '0x00'"
+            pair = uniswap_factory.functions.getPair(token_address, weth).call()
+            response = requests.get(f"{DEXSCREENER_ENDPOINT}{pair}")
+            print(response.json())
+            if response.status_code == 404:
+                print(f"Pair not found for {token_address}")
+            elif response.status_code == 500:
+                print(f"Internal server error for {token_address}")
+            else:
+                response.raise_for_status()  # Raise other HTTP-related exceptions
+
+            # handle_event(response.json(), trade)
+            await asyncio.sleep(poll_interval)
+        except requests.exceptions.ConnectionError as e:
+            print(f"Connection error: {e}")
+            # Wait for a while and then retry
+            await asyncio.sleep(poll_interval)
+        except requests.exceptions.RequestException as e:
+            print(f"Request error: {e}")
+            # Handle other request-related exceptions
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            # Handle any other unexpected exceptions
+
+
+def handle_event(event, trade):
+    print("PRICE: ", event["pairs"][0]["priceUsd"])
+    print("USER: ", trade)
+    data = {
+        "Price": Decimal(event["pairs"][0]["priceUsd"]),
+        "User": trade.user,
+        "Contract": trade.token_address.lower(),
+    }
+    # result = getprice.apply_async(args=[data], queue="tc-queue2")
+    print("Cac")
+
+def main():
+    print("Listening for events...")
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(asyncio.gather(log_loop(0, 5)))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+
+
+if __name__ == "__main__":
+    main()
