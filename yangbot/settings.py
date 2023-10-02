26524106a9ae2beeeb557c86e9a5e257dc26d40e
Jeremiah David <jeremiahedavid@gmail.com>
2023-10-02 14:16:17 +0100

    everything works

--- yangbot/settings.py
+++ yangbot/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "yangbot.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "official_db.sqlite3",# "db.sqlite3",
     }
 }
 
